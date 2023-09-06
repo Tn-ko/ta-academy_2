bc574aeb6440b03206b1e31f13693b1b52e59328
Daniil Tarasov <99136238+dantarasov@users.noreply.github.com>
2023-09-06 20:07:03 +0300

    Update createEditAcc.test.ts

--- ui/tests/createEditAcc.test.ts
+++ ui/tests/createEditAcc.test.ts
@@ -30,7 +30,7 @@ test.describe('check that we can create and edit account', () => {
             await page.getByPlaceholder('First name').fill(faker.person.firstName());
             await page.getByPlaceholder('Last name').fill(faker.person.lastName());
             await page.getByPlaceholder('Password').fill( faker.internet.password());
-            const signUp = page.locator('//button//span//span[contains(., "Sign Up")]');
+            const signUp = page.locator('//button[contains(., "Sign Up")]');
             await signUp.click();
 
         });
@@ -52,8 +52,8 @@ test.describe('check that we can create and edit account', () => {
         await test.step('go to My Account', async () => {
             const welcomeUser = page.locator('//header//button').nth(3);
             await welcomeUser.click();
-            const [window] = await page.getByLabel('Tooltip Content').all();
-            const myAccount = window.locator('[href*="/customer/account"]').first();
+            const [tooltip] = await page.getByLabel('Tooltip Content').all();
+            const myAccount = tooltip.locator('[href*="/customer/account"]').first();
             await myAccount.click();
 
         });
